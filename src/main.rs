c79b0032280b22fc923443f77899618312df023c
Jack Harper <jack.harper@stfc.ac.uk>
2025-09-02 16:25:31 +0100

    day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ fn day1() {
     let contents =
         fs::read_to_string("inputs/1.txt").expect("Something went wrong reading the file");
 
-    let blah: Vec<(u32, u32)> = contents
+    // PART 1
+    let blah: Vec<(usize, usize)> = contents
         .lines()
         .map(|s| s.split_once("   ").expect("something bad happened"))
         .map(|t| {
@@ -15,17 +16,26 @@ fn day1() {
         })
         .collect();
 
-    let mut left_vec: Vec<u32> = blah.iter().map(|t| t.0).collect();
+    let mut left_vec: Vec<usize> = blah.iter().map(|t| t.0).collect();
     left_vec.sort();
 
-    let mut right_vec: Vec<u32> = blah.iter().map(|t| t.1).collect();
+    let mut right_vec: Vec<usize> = blah.iter().map(|t| t.1).collect();
     right_vec.sort();
-    let result: u32 = left_vec
+    let result: usize = left_vec
         .iter()
         .zip(right_vec.iter())
         .map(|t| t.0.abs_diff(*t.1))
         .sum();
     println!("{result}");
+
+    // PART 2
+
+    let res: usize = left_vec
+        .iter()
+        .map(|x| right_vec.iter().filter(|&n| *n == *x).count() * x)
+        .sum();
+
+    println!("{res}");
 }
 
 fn main() {
